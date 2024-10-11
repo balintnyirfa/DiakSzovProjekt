44586f69a7b04c9b12cbd7f0e40ef28a4fc62e45
BALINT NYIRFA <balint.nyirfa@gmail.com>
2024-10-12 00:26:45 +0200

    Common styles file created

---
+++ styles/common.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+const styles2 = StyleSheet.create({
+    lightFont: {
+        fontFamily: 'Quicksand-Light',
+    },
+    regularFont: {
+        fontFamily: 'Quicksand-Regular',
+    },
+    semiBoldFont: {
+        fontFamily: 'Quicksand-SemiBold',
+    },
+    boldFont: {
+        fontFamily: 'Quicksand-Bold',
+    },
+    header: {
+        marginBottom: 13
+    },
+    regularSize: {
+        fontSize: 17
+    },
+    mediumSize: {
+        fontSize: 22,
+    },
+    whiteText: {
+        color: '#FFFFFF',
+    },
+    darkBrownColor: {
+        color: "#373B2C",
+    },
+})
+
+export default styles2;
