0accfc70bc129f248af075d654062ed7e0aa3558
BALINT NYIRFA <balint.nyirfa@gmail.com>
2024-11-03 15:54:18 +0100

    Payment calculator fix

--- screens/PaymentCalculator.js
+++ screens/PaymentCalculator.js
@@ -13,12 +13,8 @@ export default function PaymentCalculator({ navigation }) {
     const calculatePayment = () => {
         let payment = workHours * gross;
         let finalPayment = payment;
-        if (is25Selected) {
-            setTax(0.15);
-        } else {
-            setTax(0);
-        }
-        finalPayment -= (payment * tax);
+        const currentTax = is25Selected ? 0.15 : 0;
+        finalPayment -= (payment * currentTax);
         setNet(finalPayment);
     };
 
@@ -34,57 +30,70 @@ export default function PaymentCalculator({ navigation }) {
                     <Text style={[common.boldFont, common.bigSize, common.darkBrownColor]}>Bérkalkulátor</Text>
                 </View>
             </View>
-            <View style={[styles.jobs]}>
+            <View style={[styles.boxes]}>
                 <Text style={[common.regularFont, common.regularSize, styles.longText, common.darkBrownColor]}>Számold ki, hogy mennyi lesz a várható béred!</Text>
-                <Text style={[common.boldFont, common.regularSize, common.darkBrownColor, { paddingBottom: 5 }]}>Ledolgozott órák</Text>
-                <TextInput
-                    style={[common.inputField, common.borderStyle, common.darkBrownColor]}
-                    keyboardType="number-pad"
-                    onChangeText={text => setWorkHours(text)}
-                    value={String(workHours)} />
-                <Text style={[common.boldFont, common.regularSize, common.darkBrownColor, { paddingBottom: 5 }]}>Bruttó bér</Text>
-                <TextInput
-                    style={[common.inputField, common.borderStyle, common.darkBrownColor]}
-                    keyboardType="number-pad"
-                    onChangeText={text => setGross(text)}
-                    value={String(gross)} />
-                <Text style={[common.boldFont, common.regularSize, common.darkBrownColor, { paddingBottom: 5 }]}>Várható fizetés</Text>
-                <TextInput
-                    style={[common.inputField, common.borderStyle, common.darkBrownColor]}
-                    editable={false}
-                    onChangeText={text => setNet(text)}
-                    value={String(net)} />
-            </View>
-            <View style={styles.switchContainer}>
-                <View style={styles.switchContainerInside}>
-                    <Text style={[common.regularFont, common.darkBrownColor]}>25 év feletti vagyok (15%)</Text>
-                    <Switch
-                        value={is25Selected}
-                        onValueChange={set25IsSelected} />
+                <View style={[styles.box, common.borderStyle]}>
+                    <Text style={[common.regularFont, common.regularSize, common.whiteText, { paddingBottom: 5 }]}>Ledolgozott órák</Text>
+                    <TextInput
+                        style={[common.inputField, common.borderStyle, common.darkBrownColor]}
+                        keyboardType="number-pad"
+                        onChangeText={text => setWorkHours(text)}
+                        value={String(workHours)} />
+                    <Text style={[common.regularFont, common.regularSize, common.whiteText, { paddingBottom: 5 }]}>Bruttó bér</Text>
+                    <TextInput
+                        style={[common.inputField, common.borderStyle, common.darkBrownColor]}
+                        keyboardType="number-pad"
+                        onChangeText={text => setGross(text)}
+                        value={String(gross)} />
+                    <Text style={[common.regularFont, common.regularSize, common.whiteText, { paddingBottom: 5 }]}>Várható fizetés</Text>
+                    <TextInput
+                        style={[common.inputField, common.borderStyle, common.darkBrownColor]}
+                        editable={false}
+                        onChangeText={text => setNet(text)}
+                        value={String(net)} />
+                    <View style={styles.switchContainer}>
+                        <View style={styles.switchContainerInside}>
+                            <Text style={[common.regularFont, common.whiteText]}>25 év feletti vagyok (15%)</Text>
+                            <Switch
+                                value={is25Selected}
+                                onValueChange={set25IsSelected} />
+                        </View>
+                    </View>
+                    <TouchableOpacity>
+                        <Pressable onPress={calculatePayment} style={[styles.button, styles.modifyButtons]}>
+                            <Text style={[common.regularFont, common.boldFont, common.whiteText]}>SZÁMOL</Text>
+                        </Pressable>
+                    </TouchableOpacity>
                 </View>
-                <TouchableOpacity>
-                    <Pressable onPress={calculatePayment}>
-                        <Text style={[common.regularFont, common.boldFont, common.darkBrownColor]}>SZÁMOL</Text>
-                    </Pressable>
-                </TouchableOpacity>
             </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    modifyButtons: {
+        width: '100%',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 15,
+        backgroundColor: '#687A3C',
+    },
     switchContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: "space-between",
-        marginVertical: 10,
+        marginBottom: 17,
     },
     switchContainerInside: {
         flexDirection: 'row',
         alignItems: 'center',
     },
     longText: {
-        textAlign: "left",
+        textAlign: "center",
         marginBottom: 10
     },
     main: {
@@ -111,11 +120,19 @@ const styles = StyleSheet.create({
         height: 30,
         width: 30,
     },
-    jobs: {
+    boxes: {
         width: '100%',
-        flexDirection: 'Column',
         paddingHorizontal: 20,
-        paddingTop: 10
+        paddingVertical: 10
+    },
+    box: {
+        width: '100%',
+        marginVertical: 5,
+        paddingTop: 10,
+        paddingHorizontal: 18,
+        paddingBottom: 18,
+        backgroundColor: '#373B2C',
+        borderRadius: 30
     },
     inputFieldB: {
         marginBottom: 3,
